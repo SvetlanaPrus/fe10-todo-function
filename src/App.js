75f21e14cc90a1e9ce02326e04818a6f8d438557
SvetlanaPrus <64580478+SvetlanaPrus@users.noreply.github.com>
2021-03-02 04:08:59 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
 
 function App() {
+
+  const [taskInput, setTaskInput] = useState('');
+  const [statusInput, setStatusInput] = useState('');
+  const [list, setList] = useState([{
+      id: 1,
+      name: "Learn JS",
+      done: "yes",
+  },{
+      id: 2,
+      name: "Learn React",
+      done: "no",
+  }]);
+
+  const handlerOkClick = () => {
+      setList([...list, {
+          id: Math.floor(Math.random()*10),
+          name: taskInput,
+          done: statusInput,
+      }]);
+      setTaskInput('');
+      setStatusInput('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <h2 className="h2"> TodoList & function </h2>
+        <input placeholder="Name of task..." value={taskInput} onChange={(e) => setTaskInput(e.target.value)} type="text"/>
+        <input placeholder="Done? ...yes/no" value={statusInput} onChange={(e) => setStatusInput(e.target.value)} type="text"/>
+        <button onClick={handlerOkClick}> Ok </button>
+          <ul>
+            {
+                list.map(el => <li key={el.id} className={el.done === "yes"? "done" : "not-done"}>{el.name}</li>)
+            }</ul>
+      </div>
   );
 }
 
